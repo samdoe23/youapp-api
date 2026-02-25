d6ba20d5116078a0ee8a5c797ea86035d7c467ab
arsmoriendy <nugrahasteven@gmail.com>
2026-02-25 15:32:31 +0700

    chore: format

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ProfileModule } from './profile/profile.module';
+import { ProfileModule } from "./profile/profile.module";
 
 @Module({
   imports: [ProfileModule],
