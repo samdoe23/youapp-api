a2a30d962ac47f39ce31ef739e6b8d169c679e1f
arsmoriendy <nugrahasteven@gmail.com>
2026-02-25 15:28:15 +0700

    chore: init profile resource

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Patch, Param } from "@nestjs/common";
+import { ProfileService } from "./profile.service";
+import { CreateProfileDto } from "./dto/create-profile.dto";
+import { UpdateProfileDto } from "./dto/update-profile.dto";
+
+@Controller()
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Post("createProfile")
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return this.profileService.create(createProfileDto);
+  }
+
+  @Get("getProfile:id")
+  findOne(@Param("id") id: string) {
+    return this.profileService.findOne(+id);
+  }
+
+  @Patch("updateProfile:id")
+  update(@Param("id") id: string, @Body() updateProfileDto: UpdateProfileDto) {
+    return this.profileService.update(+id, updateProfileDto);
+  }
+}
---
+++ src/profile/profile.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { CreateProfileDto } from "./dto/create-profile.dto";
+import { UpdateProfileDto } from "./dto/update-profile.dto";
+
+@Injectable()
+export class ProfileService {
+  create(createProfileDto: CreateProfileDto) {
+    return "This action adds a new profile";
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} profile`;
+  }
+
+  update(id: number, updateProfileDto: UpdateProfileDto) {
+    return `This action updates a #${id} profile`;
+  }
+}
