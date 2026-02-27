f97dd2bc1c083eb7e938173bc3dcb89418529c51
arsmoriendy <nugrahasteven@gmail.com>
2026-02-27 12:03:26 +0700

    chore: delete unused app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { ProfileModule } from "./profile/profile.module";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import * as Joi from "joi";
@@ -25,7 +23,5 @@ import { AuthModule } from "./auth/auth.module";
     ProfileModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
