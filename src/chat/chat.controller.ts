ed7a48eb2ee5d18e630e1581dbd1acface3e5147
arsmoriendy <nugrahasteven@gmail.com>
2026-03-03 16:22:16 +0700

    fix: confirm user exists in chat controller

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,15 +1,24 @@
-import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+  Req,
+  UseGuards,
+} from "@nestjs/common";
 import { Types } from "mongoose";
 import { ChatGateway } from "src/chat/chat.gateway";
 import { ChatService } from "src/chat/chat.service";
 import { SendMessageDto } from "src/chat/dtos/send-message.dto";
 import { JwtGuard } from "src/jwt/jwt.guard";
 import { Payload } from "src/jwt/jwt.payload";
+import { UserService } from "src/user/user.service";
 
 @Controller("")
 @UseGuards(JwtGuard)
 export class ChatController {
   constructor(
+    private readonly userService: UserService,
     private readonly chatService: ChatService,
     private readonly chatGateway: ChatGateway,
   ) {}
@@ -19,9 +28,18 @@ export class ChatController {
     @Body() message: SendMessageDto,
     @Req() req: Request & { user: Payload },
   ) {
-    const roomId = await this.chatService.saveMessage(
+    const participants: [by: Types.ObjectId, to: Types.ObjectId] = [
       new Types.ObjectId(req.user.sub),
       new Types.ObjectId(message.to),
+    ];
+
+    for (const p of participants) {
+      const userExists = await this.userService.exists(p);
+      if (!userExists) throw new NotFoundException("user doesn't exist");
+    }
+
+    const roomId = await this.chatService.saveMessage(
+      ...participants,
       message.content,
     );
 
