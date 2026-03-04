f072358f3fe4989ac061dd6e409f9321e885cd07
arsmoriendy <nugrahasteven@gmail.com>
2026-03-04 09:57:10 +0700

    chore: remove dangling log

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -28,7 +28,6 @@ export class ChatService {
   }
 
   async getMessages(participants: Types.ObjectId[]) {
-    console.log(participants);
     const room = await this.roomModel.findOne({
       participants: { $in: participants, $size: 2 },
     });
